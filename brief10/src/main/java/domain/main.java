42bcc14482e6476390ced00c5d4ecb6513e5002f
Elon-Fask <aatman.aboulaajala04@gmail.com>
2022-07-19 17:58:41 +0100

    main

---
+++ brief10/src/main/java/domain/main.java
@@ -0,0 +1,21 @@
+package domain;
+
+import java.sql.Connection;
+import java.util.Scanner;
+import 
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner reader = new Scanner(System.in);
+		String test;
+		
+		System.out.println("Hello Admin, Please do Log into your dashboard !");
+		test = reader.nextLine();
+		System.out.println("your Password");
+		
+	}
+
+}
